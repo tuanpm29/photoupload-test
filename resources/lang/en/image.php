3ce867ec6bf27485d67b39ac313373e10d688d05
Tuan Pham <tuanpm.2909@gmail.com>
2019-02-28 14:13:51 +0700

    Lang for image related functions

---
+++ resources/lang/en/image.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'error' => [
+        'upload' => [
+            'required' => 'Image is required',
+            'image' => 'Uploaded file is not an image'
+        ],
+        'not_found' => 'Image not found'
+    ]
+];
